20798ac194c4e1f395e68633b41ca3aa4d5dd41f
imhess <79474657+imhess@users.noreply.github.com>
2023-02-09 12:11:34 -0800

    Test Commit - Goodbye World

--- helloWorld/helloWorldApp/views.py
+++ helloWorld/helloWorldApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello(request):
-    return HttpResponse('<h1 style="color:red">Hello World!!</h1>')
+    return HttpResponse('<h1 style="color:red">Goodbye World!!</h1>')
