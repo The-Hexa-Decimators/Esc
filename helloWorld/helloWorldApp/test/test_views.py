5ca690bc184d89b5d01d146a2b71466124c81e04
imhess <79474657+imhess@users.noreply.github.com>
2023-03-15 15:13:37 -0700

    Working Test - View Edition (Official)
    Test case for home view is functional

--- helloWorld/helloWorldApp/test/test_views.py
+++ helloWorld/helloWorldApp/test/test_views.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class ViewsTestCase(TestCase):
     def test_index_loads_properly(self):
-        """The index page loads properly"""
-        response = self.client.get('127.0.0.1:8000')
-        self.assertEqual(response.status_code, 200)
+        """The page loads properly"""
+        url = reverse("home_view")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
