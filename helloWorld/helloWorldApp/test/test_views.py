5eb0ebd723008d587abdbd02878fbba945b6743e
Jonathan Peña <jonathanpena@b01-aruba-authenticated-10-110-220-244.wlan.cpp.edu>
2023-03-16 15:16:03 -0700

    A6

--- helloWorld/helloWorldApp/test/test_views.py
+++ helloWorld/helloWorldApp/test/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 
 class ViewsTestCase(TestCase):
@@ -7,3 +7,10 @@ class ViewsTestCase(TestCase):
         url = reverse("home_view")
         resp = self.client.get(url)
         self.assertEqual(resp.status_code, 200)
+
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The page loads properly"""
+        url = reverse("home")
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200) 
