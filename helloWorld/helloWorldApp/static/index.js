ae10da82b8220feaac604715398a767bb55fb723
JonathanPena17 <jonathanrpena23@gmail.com>
2023-05-04 13:12:15 -0700

    I hope this works

--- helloWorld/helloWorldApp/static/index.js
+++ helloWorld/helloWorldApp/static/index.js
@@ -1,66 +1,53 @@
 function initMap() {
-	// Set the default map center to the user's location
-	if (navigator.geolocation) {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-		var userLocation = {
-		  lat: position.coords.latitude,
-		  lng: position.coords.longitude
-		};
-		var map = new google.maps.Map(document.getElementById("map"), {
-		  center: userLocation,
-		  zoom: 10,
-		  mapTypeId: "roadmap",
-		});
-		loadImages(userLocation.lat, userLocation.lng, "Your location: " + userLocation.lat + ", " + userLocation.lng);
+	// Set the default map center to Cal Poly Pomona
+	var cpPomona = { lat: 34.0575, lng: -117.8217 };
+	var map = new google.maps.Map(document.getElementById("map"), {
+	  center: cpPomona,
+	  zoom: 10,
+	  mapTypeId: "roadmap",
+	});
   
-		// Create the Autocomplete input field and restrict to cities
-		autocomplete = new google.maps.places.Autocomplete(
-		  document.getElementById("location"),
-		  {
-			types: ["(cities)"],
-		  }
-		);
-		// When the user selects a city, update the map center and search for escape rooms
-		autocomplete.addListener("place_changed", function() {
-		  var place = autocomplete.getPlace();
-		  if (place.geometry) {
-			map.setCenter(place.geometry.location);
-			map.setZoom(13);
-			var location = place.formatted_address;
-			loadImages(place.geometry.location.lat(), place.geometry.location.lng(), location);
-			$.ajax({
-			  url: "escape_rooms/",
-			  data: { location: location },
-			  success: function(data) {
-				var escapeRooms = "";
-				var markers = [];
-				for (var i = 0; i < data.length; i++) {
-				  var escapeRoom = data[i].location + " " + location;
-				  var name = data[i].name;
-				  var rating = data[i].rating;
-				  var phone = data[i].phone;
-				  var url = data[i].url;
-				  var geocoder = new google.maps.Geocoder();
-				  geocodeAndPlaceMarker(geocoder, map, markers, escapeRoom, name, rating, phone, url, escapeRooms);
-				}
-				$("#escape-rooms").html(escapeRooms);
-
-			  },
-			});
-		  } else {
-			alert("No details available for input: '" + place.name + "'");
-		  }
+	// Create the Autocomplete input field and restrict to cities
+	autocomplete = new google.maps.places.Autocomplete(
+	  document.getElementById("location"),
+	  {
+		types: ["(cities)"],
+	  }
+	);
+	
+	// When the user selects a city, update the map center and search for escape rooms
+	autocomplete.addListener("place_changed", function() {
+	  var place = autocomplete.getPlace();
+	  if (place.geometry) {
+		map.setCenter(place.geometry.location);
+		map.setZoom(13);
+		var location = place.formatted_address;
+		loadImages(place.geometry.location.lat(), place.geometry.location.lng(), location);
+		$.ajax({
+		  url: "escape_rooms/",
+		  data: { location: location },
+		  success: function(data) {
+			var escapeRooms = "";
+			var markers = [];
+			for (var i = 0; i < data.length; i++) {
+			  var escapeRoom = data[i].location + " " + location;
+			  var name = data[i].name;
+			  var rating = data[i].rating;
+			  var phone = data[i].phone;
+			  var url = data[i].url;
+			  var geocoder = new google.maps.Geocoder();
+			  geocodeAndPlaceMarker(geocoder, map, markers, escapeRoom, name, rating, phone, url, escapeRooms);
+			}
+			$("#escape-rooms").html(escapeRooms);
+  
+		  },
 		});
-		// Load escape room images for the user's location
-		loadImages(userLocation.lat, userLocation.lng);
-	  });
-	} else {
-	  var map = new google.maps.Map(document.getElementById("map"), {
-		center: { lat: 37.7749, lng: -122.4194 }, // Default center if geolocation not supported
-		zoom: 13,
-		mapTypeId: "roadmap",
-	  });
-	}
+	  } else {
+		alert("No details available for input: '" + place.name + "'");
+	  }
+	});
+	// Load escape room images for Cal Poly Pomona
+	loadImages(cpPomona.lat, cpPomona.lng, "Cal Poly Pomona");
   }
   
   function geocodeAndPlaceMarker(geocoder, map, markers, escapeRoom, name, rating, phone, url, escapeRooms) {
@@ -87,8 +74,7 @@ function initMap() {
 		alert("Geocode was not successful for the following reason: " + status);
 	  }
 	});
-  }
-
+  }  
   function loadImages(lat, lng, location) {
 	fetch(`escape_rooms/?latitude=${lat}&longitude=${lng}&location=${location}`)
 	  .then(response => response.json())
